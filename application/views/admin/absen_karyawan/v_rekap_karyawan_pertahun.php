2246ac5c2b84e4ed306c7bafdd8aacfb168787a8
Muzakir Nur <muzakir.nurr@gmail.com>
2022-03-04 04:01:58 +0700

    MUZAKIR YANG BETOLIN NI BU

--- application/views/admin/absen_karyawan/v_rekap_karyawan_pertahun.php
+++ application/views/admin/absen_karyawan/v_rekap_karyawan_pertahun.php
@@ -5,7 +5,7 @@
     </div>
     <div class="card-body ">
 
-      <br/>
+      <br />
 
       <div class="row justify-content-center">
         <div class="col-md-4">
@@ -15,25 +15,26 @@
             </div>
             <div class="card-body">
 
-            <form method="post" action="<?php echo base_url().'anggota/karyawan_pertahun'?>">
-						<div class="form-group">
-          <label class="font-weight-bold" >Karyawan</label>
-          <select name="karyawan" id="karyawan" class="form-control">
-            <option value="">- Pilih Karyawan</option>
-            <?php
-					foreach($karyawan as $data){ // Lakukan looping pada variabel siswa dari controller
-						echo "<option value='".$data->id."'>".$data->nama."</option>";
-					}
-					?>
-          </select>
-        </div>
-              <div class="form-group">
-              <label class="font-weight-bold" >Tahun</label>
+              <form method="post" action="<?php echo base_url() . 'anggota/karyawan_pertahun' ?>">
+                <div class="form-group">
+                  <label class="font-weight-bold">Karyawan</label>
+                  <select name="karyawan" id="karyawan" class="form-control">
+                    <option value="">- Pilih Karyawan</option>
+                    <option value="all">Semua Karyawan</option>
+                    <?php
+                    foreach ($karyawan as $data) { // Lakukan looping pada variabel siswa dari controller
+                      echo "<option value='" . $data->id . "'>" . $data->nama . "</option>";
+                    }
+                    ?>
+                  </select>
+                </div>
+                <div class="form-group">
+                  <label class="font-weight-bold">Tahun</label>
 
-              <input type="text" class="form-control" name="tanggal" id="datepicker" /> 
-</div> 
-              
-               <center> <input type="submit" class="btn btn-primary" value="Filter"></center>
+                  <input type="text" class="form-control" name="tanggal" id="datepicker" />
+                </div>
+
+                <center> <input type="submit" class="btn btn-primary" value="Filter"></center>
               </form>
 
             </div>
@@ -42,16 +43,16 @@
       </div>
 
 
-      
-    
-  <!-- Bootstrap Date-Picker Plugin -->
-<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css"/>
-  
-<script>
-   $("#datepicker").datepicker( {
-    format: "yyyy",
-    viewMode: "years", 
-    minViewMode: "years"
-});
-</script>
+
+
+      <!-- Bootstrap Date-Picker Plugin -->
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/js/bootstrap-datepicker.min.js"></script>
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.4.1/css/bootstrap-datepicker3.css" />
+
+      <script>
+        $("#datepicker").datepicker({
+          format: "yyyy",
+          viewMode: "years",
+          minViewMode: "years"
+        });
+      </script>
